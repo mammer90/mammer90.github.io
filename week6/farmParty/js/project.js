9bcac8baf1e1c2489be91589a00f9fd9fa2062c4
mammer90 <User@ool-3f8fe17f.dyn.optonline.net>
2015-08-12 14:03:01 -0400

    Farm Party
    The farm is partying!

---
+++ week6/farmParty/js/project.js
@@ -0,0 +1,74 @@
+$(document).ready(function(){
+$("#jumpman").mouseover(function() {
+  $(this).animate({
+    "top": "50%",
+    "left": "60%"
+}, 4000);
+});
+});
+$("#cow41").mouseover(function() {
+  $(this).animate({
+    "top": "50%",
+    "left": "70%"
+}, 3000);
+});
+$("#cow1").mouseover(function() {
+  $(this).animate({
+    "bottom": "20%",
+    "right": "40%"
+}, 2500);
+});
+$("#dancingcow").mouseover(function() {
+  $(this).animate({
+    "top": "45%",
+    "left": "70%"
+}, 2700);
+});
+$("#farm1").mouseover(function() {
+  $(this).animate({
+    "bottom": "25%",
+    "right": "55%"
+}, 3500);
+});
+$("#horse").mouseover(function() {
+  $(this).animate({
+    "bottom": "25%",
+    "right": "75%"
+}, 2500);
+});
+$("#lamb").mouseover(function() {
+  $(this).animate({
+    "bottom": "29%",
+    "right": "37%"
+}, 2750);
+});
+$("#roosterdance").mouseover(function() {
+  $(this).animate({
+    "bottom": "18%",
+    "right": "56%"
+}, 3750);
+});
+$("#varken").mouseover(function() {
+  $(this).animate({
+    "bottom": "4%",
+    "right": "72%"
+}, 2250);
+});
+$("#roosterfoot").mouseover(function() {
+  $(this).animate({
+    "bottom": "30%",
+    "right": "70%"
+}, 4450);
+});
+$("#sheep").mouseover(function() {
+  $(this).animate({
+    "top": "70%",
+    "right": "80%"
+}, 3450);
+});
+$("#sun").mouseover(function() {
+  $(this).animate({
+    "bottom": "80%",
+    "left": "80%"
+}, 5450);
+});
